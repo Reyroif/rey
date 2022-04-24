e914462662be689b87a7f679ba0bf08c3c2060c6
Reyroif <97400728+Reyroif@users.noreply.github.com>
2022-04-24 13:19:30 +0700

    Update roif1.py

--- roif1.py
+++ roif1.py
@@ -8,8 +8,3 @@ def __main__():
   except Exception as e:
     exit(f"\x1b[1;97m[\x1b[1;91m!\x1b[1;97m]\x1b[1;91m {e}")
 
-if __name__=='__main__':
-  if '64bit' in str(platform.architecture()):
-    os.system('git pull');__main__()
-  else:
-    exit("\x1b[1;97m[\x1b[1;91m!\x1b[1;97m]\x1b[1;91m Gunakan Perangkat 64bit")
